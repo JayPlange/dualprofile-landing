46a2d67bd5f0dddaa1546e605bacc4f421d99931
Edwin Plange <edwin.dualprofile@gmail.com>
2026-05-11 06:03:24 +0000

    Fix: self-close Kit.com script tag for JSX compatibility

--- pages/index.tsx
+++ pages/index.tsx
@@ -124,7 +124,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.png" />
         <link rel="apple-touch-icon" href="/favicon.png" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <script async data-uid="45b1efe5a4" src="https://dualprofile.kit.com/45b1efe5a4/index.js"></script>
+        <script async data-uid="45b1efe5a4" src="https://dualprofile.kit.com/45b1efe5a4/index.js" />
       </Head>
 
       <div className="app">
